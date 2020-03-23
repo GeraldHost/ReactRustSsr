98a6bf7224deedea37d2e59d79a6f9a37d66bc70
Gerald <me@jacobford.co.uk>
2020-03-23 08:20:31 +0000

    feat(server/docker): update dockerfile and env

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,21 @@
+#![allow(non_camel_case_types)]
+
 use hello_world::render_client::RenderClient;
-use hello_world::RenderReply;
 use hello_world::RenderRequest;
 
-use tonic::transport::Channel;
-
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-
-use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
 
 async fn handler(req: HttpRequest) -> Result<HttpResponse, Error> {
-    let mut client = RenderClient::connect("http://[::1]:50051")
+    let grpc_host = env!("GRPC_HOST");
+    let grpc_port = env!("GRPC_PORT");
+    let grpc_connect = format!("http://{}:{}", grpc_host, grpc_port);
+    let mut client = RenderClient::connect(grpc_connect)
         .await
-        .unwrap("GRPc: Connection error");
+        .expect("GRPc: Connection error");
 
     let route_path = String::from(req.match_info().path());
     let request = tonic::Request::new(RenderRequest { path: route_path });
@@ -25,7 +23,7 @@ async fn handler(req: HttpRequest) -> Result<HttpResponse, Error> {
     let response = client
         .render(request)
         .await
-        .unwrap("GRPc: Response error")
+        .expect("GRPc: Response error")
         .into_inner();
 
     Ok(HttpResponse::Ok().body(response.html))
@@ -33,8 +31,11 @@ async fn handler(req: HttpRequest) -> Result<HttpResponse, Error> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let host = env!("HOST");
+    let port = env!("PORT");
+    let http_bind = format!("{}:{}", host, port);
     HttpServer::new(|| App::new().default_service(web::route().to(handler)))
-        .bind("127.0.0.1:8080")?
+        .bind(http_bind)?
         .run()
         .await
 }
